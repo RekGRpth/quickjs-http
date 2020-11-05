31df51f1c9bd1e57af4a92687b7dd1fa50610f25
RekGRpth <rekgrpth@gmail.com>
2020-11-05 12:11:39 +0500

    up

--- http.js
+++ http.js
@@ -8,15 +8,8 @@ http.setsockopt(server.fd, http.SOL_SOCKET, http.SO_REUSEPORT, 1)
 server.af = http.bind(server.fd, server.host, server.port)
 http.listen(server.fd, http.SOMAXCONN)
 console.log(JSON.stringify(server))
-let time, rTEXT
-function update() {
-    time = (new Date()).toUTCString()
-    rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${time}\r\nContent-Type: text/plain\r\nContent-Length: `
-//    os.setTimeout(update, 100)
-}
 const text = 'Hello, World!'
 const END = '\r\n\r\n'
-update()
 while (true) {
     const client = http.accept(server.fd, server.af)
     console.log(JSON.stringify(client))
@@ -24,8 +17,10 @@ while (true) {
     http.setsockopt(client.fd, http.SOL_SOCKET, http.SO_KEEPALIVE, 0)
     while (true) {
         const request = http.recv(client.fd, 128, 0)
-        if (!request || !request.length) break
+        if (!request.length) break
         console.log(JSON.stringify({request: request}))
+        const time = (new Date()).toUTCString()
+        const rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${time}\r\nContent-Type: text/plain\r\nContent-Length: `
         const response = `${rTEXT}${text.length}${END}${text}`
         console.log(JSON.stringify({response: response}))
         http.send(client.fd, response, http.MSG_NOSIGNAL)
