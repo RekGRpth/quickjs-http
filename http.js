dd456860fd5c49c91c5598d8a16c40189a6ad2e3
RekGRpth <rekgrpth@gmail.com>
2020-11-05 15:32:23 +0500

    up

--- http.js
+++ http.js
@@ -21,22 +21,23 @@ const END = '\r\n\r\n'
 let client
 while (true) try {
     client = http.accept(server.fd, server.af)
-//    console.log(JSON.stringify(client))
-    http.setsockopt(client.fd, http.IPPROTO_TCP, http.TCP_NODELAY, 0)
-    http.setsockopt(client.fd, http.SOL_SOCKET, http.SO_KEEPALIVE, 0)
+    console.log(JSON.stringify(client))
+    http.setsockopt(client.fd, http.IPPROTO_TCP, http.TCP_NODELAY, 1)
+    http.setsockopt(client.fd, http.SOL_SOCKET, http.SO_KEEPALIVE, 1)
     while (true) {
         const request = http.recv(client.fd, 128, 0)
         if (!request.length) break
-//        console.log(JSON.stringify({request: request}))
+        console.log(JSON.stringify({client: client, request: request}))
         const time = (new Date()).toUTCString()
         const rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${time}\r\nContent-Type: text/plain\r\nContent-Length: `
         const response = `${rTEXT}${text.length}${END}${text}`
-//        console.log(JSON.stringify({response: response}))
+        console.log(JSON.stringify({client: client, response: response}))
         http.send(client.fd, response, http.MSG_NOSIGNAL)
     }
 } catch(e) {
     const time = (new Date()).toUTCString()
-    console.log(`${time} ${e}`);
+    const client_str = JSON.stringify(client)
+    console.log(`${client_str} ${time} ${e}`);
     console.log((e?.stack || "").replace(/^/mg, time));
 } finally {
     os.close(client.fd)
