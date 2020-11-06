417e8b438afbf8d7998666d080a0caee65484b49
RekGRpth <rekgrpth@gmail.com>
2020-11-06 09:56:21 +0500

    up

--- http.js
+++ http.js
@@ -1,41 +1,42 @@
 import * as http from "http.so"
 import * as os from "os"
 import * as std from "std"
-let time, rTEXT
-function update() {
-    time = (new Date()).toUTCString()
-    rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${time}\r\nContent-Type: text/plain\r\nContent-Length: `
-    os.setTimeout(update, 1000)
+const server = {
+    host: '0.0.0.0',
+    port: 8080,
+    text: 'Hello, World!',
+    END: '\r\n\r\n',
+    update: () => {
+        server.time = (new Date()).toUTCString()
+        server.rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${server.time}\r\nContent-Type: text/plain\r\nContent-Length: `
+        os.setTimeout(server.update, 1000)
+    },
 }
-const text = 'Hello, World!'
-const END = '\r\n\r\n'
-update()
-const server = { host: '0.0.0.0', port: 8080 }
 server.fd = http.socket(http.AF_INET, http.SOCK_STREAM | http.SOCK_NONBLOCK, 0)
 http.setsockopt(server.fd, http.SOL_SOCKET, http.SO_REUSEADDR, 1)
 http.setsockopt(server.fd, http.SOL_SOCKET, http.SO_REUSEPORT, 1)
 server.af = http.bind(server.fd, server.host, server.port)
 //console.log(JSON.stringify(http))
 http.listen(server.fd, http.SOMAXCONN)
+server.update()
 console.log(JSON.stringify(server))
 os.setReadHandler(server.fd, () => { /*try {*/
     const client = http.accept(server.fd, server.af)
-    console.log(JSON.stringify({time: time, client: client}))
+    console.log(JSON.stringify({server: server, client: client}))
     http.setsockopt(client.fd, http.IPPROTO_TCP, http.TCP_NODELAY, 0)
     http.setsockopt(client.fd, http.SOL_SOCKET, http.SO_KEEPALIVE, 0)
     os.setReadHandler(client.fd, () => { try {
-        const request = http.recv(client.fd, 128, 0)
-        if (request.length) {
-            console.log(JSON.stringify({time: time, client: client, request: request}))
-            const response = `${rTEXT}${text.length}${END}${text}`
-            console.log(JSON.stringify({time: time, client: client, response: response}))
-            http.send(client.fd, response, http.MSG_NOSIGNAL)
+        client.request = http.recv(client.fd, 128, 0)
+        if (client.request.length) {
+            client.response = `${server.rTEXT}${server.text.length}${server.END}${server.text}`
+            console.log(JSON.stringify({server: server, client: client}))
+            http.send(client.fd, client.response, http.MSG_NOSIGNAL)
         } else {
             os.setReadHandler(client.fd, null)
             os.close(client.fd)
         }
     } catch (exception) {
-        console.log(JSON.stringify({time: time, client: client, exception: exception.message/*, stack: exception?.stack*/}));
+        console.log(JSON.stringify({server: server, client: client, exception: exception.message/*, stack: exception?.stack*/}));
 //        console.log((e?.stack || "").replace(/^/mg, time));
         os.setReadHandler(client.fd, null)
         os.close(client.fd)
