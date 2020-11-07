3c82c938f8253c7f393a86e2ae10d958514c3461
RekGRpth <rekgrpth@gmail.com>
2020-11-07 15:32:38 +0500

    up

--- http.c
+++ http.c
@@ -16,100 +16,55 @@
 //#include <sys/wait.h>
 #include <unistd.h>
 
+static JSValue js_sockaddr_to_value(JSContext *ctx, const struct sockaddr *addr) {
+    JSValue value = JS_NewObject(ctx);
+    if (JS_IsException(value)) return value;
+    switch (addr->sa_family) {
+        case AF_INET: {
+            struct sockaddr_in *addr4 = (struct sockaddr_in *)addr;
+            char buf[INET_ADDRSTRLEN];
+            if (inet_ntop(AF_INET, &addr4->sin_addr, (char *__restrict)&buf, INET_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, value, "ip", JS_NewString(ctx, buf), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "port", JS_NewInt32(ctx, ntohs(addr4->sin_port)), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "family", JS_NewInt32(ctx, AF_INET), JS_PROP_C_W_E);
+        } break;
+        case AF_INET6: {
+            struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)addr;
+            char buf[INET6_ADDRSTRLEN];
+            if (inet_ntop(AF_INET6, &addr6->sin6_addr, (char *__restrict)&buf, INET6_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, value, "ip", JS_NewString(ctx, buf), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "port", JS_NewInt32(ctx, ntohs(addr6->sin6_port)), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "family", JS_NewInt32(ctx, AF_INET6), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "flowinfo", JS_NewInt32(ctx, ntohl(addr6->sin6_flowinfo)), JS_PROP_C_W_E);
+            JS_DefinePropertyValueStr(ctx, value, "scope_id", JS_NewInt32(ctx, addr6->sin6_scope_id), JS_PROP_C_W_E);
+        } break;
+        default: return JS_ThrowInternalError(ctx, "sa_family = %i", addr->sa_family);
+    }
+    return value;
+}
+
 static JSValue js_accept(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
-    int fd, new_fd = -1;
+    int fd;
+    if (JS_ToInt32(ctx, &fd, argv[0])) return JS_EXCEPTION;
+    struct sockaddr_storage addr;
+    socklen_t addrlen;
+    int new_fd = accept(fd, (struct sockaddr *)&addr, &addrlen);
+    if (new_fd < 0) return JS_ThrowInternalError(ctx, "accept(%i): %m", fd);
+    int val = 0;
+    if (setsockopt(new_fd, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val)) < 0) return JS_ThrowInternalError(ctx, "setsockopt(%i, %i, %i, %i): %m", new_fd, IPPROTO_TCP, TCP_NODELAY, val);
+    if (setsockopt(new_fd, SOL_SOCKET, SO_KEEPALIVE, &val, sizeof(val)) < 0) return JS_ThrowInternalError(ctx, "setsockopt(%i, %i, %i, %i): %m", new_fd, IPPROTO_TCP, TCP_NODELAY, val);
+    JSValue value = js_sockaddr_to_value(ctx, (struct sockaddr *)&addr);
+    if (JS_IsException(value)) return value;
+    JS_DefinePropertyValueStr(ctx, value, "fd", JS_NewInt32(ctx, new_fd), JS_PROP_C_W_E);
+    return value;
+}
+
+static JSValue js_getpeername(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+    int fd;
     if (JS_ToInt32(ctx, &fd, argv[0])) return JS_EXCEPTION;
     struct sockaddr addr;
     socklen_t addrlen;
-    int rc = getsockname(fd, &addr, &addrlen);
-    if (rc < 0) return JS_ThrowInternalError(ctx, "getsockname(%i): %m", fd);
-    struct sockaddr_in addr4;
-    struct sockaddr_in6 addr6;
-    switch (addr.sa_family) {
-        case AF_INET: {
-            socklen_t addr4len;
-            new_fd = accept(fd, (struct sockaddr *)&addr4, &addr4len);
-        } break;
-        case AF_INET6: {
-            socklen_t addr6len;
-            new_fd = accept(fd, (struct sockaddr *)&addr6, &addr6len);
-        } break;
-        default: return JS_UNDEFINED;
-    }
-    if (new_fd < 0) return JS_ThrowInternalError(ctx, "accept(%i): %m", fd);
-    JSValue obj = JS_NewObject(ctx);
-    if (JS_IsException(obj)) return obj;
-    JS_DefinePropertyValueStr(ctx, obj, "fd", JS_NewInt32(ctx, new_fd), JS_PROP_C_W_E);
-    switch (addr.sa_family) {
-        case AF_INET: {
-            char buf[INET_ADDRSTRLEN];
-            if (inet_ntop(AF_INET, &addr4.sin_addr, (char *__restrict)&buf, INET_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, obj, "host", JS_NewString(ctx, buf), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "port", JS_NewInt32(ctx, ntohs(addr4.sin_port)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "family", JS_NewInt32(ctx, AF_INET), JS_PROP_C_W_E);
-        } break;
-        case AF_INET6: {
-            char buf[INET6_ADDRSTRLEN];
-            if (inet_ntop(AF_INET6, &addr6.sin6_addr, (char *__restrict)&buf, INET6_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, obj, "host", JS_NewString(ctx, buf), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "port", JS_NewInt32(ctx, ntohs(addr6.sin6_port)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "family", JS_NewInt32(ctx, AF_INET6), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "flowinfo", JS_NewInt32(ctx, ntohl(addr6.sin6_flowinfo)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "scope_id", JS_NewInt32(ctx, addr6.sin6_scope_id), JS_PROP_C_W_E);
-        } break;
-    }
-    return obj;
-}
-
-static int resolve_host(const char *name_or_ip, int port, struct sockaddr_in *addr, struct sockaddr_in6 *addr6) {
-    port = htons(port); // port in network order
-    if (inet_pton(AF_INET, name_or_ip, &addr->sin_addr)) {
-        memset(addr6, 0, sizeof(*addr6));
-        addr->sin_family = AF_INET;
-        addr->sin_port = port;
-    } else if (inet_pton(AF_INET6, name_or_ip, &addr6->sin6_addr)) {
-        memset(addr, 0, sizeof(*addr));
-        addr6->sin6_family = AF_INET6;
-        addr6->sin6_port = port;
-    } else {
-        struct addrinfo hints, *ret;
-        memset(&hints, 0, sizeof(hints));
-        memset(addr, 0, sizeof(*addr));
-        memset(addr6, 0, sizeof(*addr6));
-        hints.ai_socktype = SOCK_STREAM;
-        if (getaddrinfo(name_or_ip, NULL, &hints, &ret)) return -1;
-        for (struct addrinfo *cur = ret; cur; cur = cur->ai_next) {
-            if (cur->ai_addr->sa_family == AF_INET) {
-                memcpy(addr, cur->ai_addr, sizeof(*addr));
-                addr->sin_port = port;
-            } else if (cur->ai_addr->sa_family == AF_INET6) {
-                memcpy(addr6, cur->ai_addr, sizeof(*addr6));
-                addr6->sin6_port = port;
-            }
-        }
-        freeaddrinfo(ret);
-    }
-    return 0;
-}
-
-static JSValue js_bind(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
-    int fd, rc = -1, family = 0;
-    if (JS_ToInt32(ctx, &fd, argv[0])) return JS_EXCEPTION;
-    int port;
-    if (JS_ToInt32(ctx, &port, argv[2])) return JS_EXCEPTION;
-    const char *host = JS_ToCString(ctx, argv[1]);
-    if (!host) return JS_EXCEPTION;
-    struct sockaddr_in addr4;
-    struct sockaddr_in6 addr6;
-    if (!resolve_host(host, port, &addr4, &addr6)) {
-        if (addr4.sin_family == AF_INET) family = AF_INET;
-        else if (addr6.sin6_family == AF_INET6) family = AF_INET6;
-    }
-    JS_FreeCString(ctx, host);
-    switch (family) {
-        case AF_INET: rc = bind(fd, (struct sockaddr *)&addr4, sizeof(addr4)); break;
-        case AF_INET6: rc = bind(fd, (struct sockaddr *)&addr6, sizeof(addr6)); break;
-    }
-    if (rc < 0) return JS_ThrowInternalError(ctx, "bind(%i): %m", fd);
-    return JS_NewInt32(ctx, family);
+    int rc = getpeername(fd, &addr, &addrlen);
+    if (rc < 0) return JS_ThrowInternalError(ctx, "getpeername(%i), %m", fd);
+    return js_sockaddr_to_value(ctx, &addr);
 }
 
 static JSValue js_getsockname(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
@@ -119,38 +74,52 @@ static JSValue js_getsockname(JSContext *ctx, JSValueConst this_val, int argc, J
     socklen_t addrlen;
     int rc = getsockname(fd, &addr, &addrlen);
     if (rc < 0) return JS_ThrowInternalError(ctx, "getsockname(%i), %m", fd);
-    JSValue obj = JS_NewObject(ctx);
-    if (JS_IsException(obj)) return obj;
-    switch (addr.sa_family) {
-        case AF_INET: {
-            struct sockaddr_in *addr4 = (struct sockaddr_in *)&addr;
-            char buf[INET_ADDRSTRLEN];
-            if (inet_ntop(AF_INET, &addr4->sin_addr, (char *__restrict)&buf, INET_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, obj, "host", JS_NewString(ctx, buf), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "port", JS_NewInt32(ctx, ntohs(addr4->sin_port)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "family", JS_NewInt32(ctx, AF_INET), JS_PROP_C_W_E);
-        } break;
-        case AF_INET6: {
-            struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)&addr;
-            char buf[INET6_ADDRSTRLEN];
-            if (inet_ntop(AF_INET6, &addr6->sin6_addr, (char *__restrict)&buf, INET6_ADDRSTRLEN)) JS_DefinePropertyValueStr(ctx, obj, "host", JS_NewString(ctx, buf), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "port", JS_NewInt32(ctx, ntohs(addr6->sin6_port)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "family", JS_NewInt32(ctx, AF_INET6), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "flowinfo", JS_NewInt32(ctx, ntohl(addr6->sin6_flowinfo)), JS_PROP_C_W_E);
-            JS_DefinePropertyValueStr(ctx, obj, "scope_id", JS_NewInt32(ctx, addr6->sin6_scope_id), JS_PROP_C_W_E);
-        } break;
-        default: return JS_UNDEFINED;
-    }
-    return obj;
+    return js_sockaddr_to_value(ctx, &addr);
 }
 
 static JSValue js_listen(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
-    int fd;
-    if (JS_ToInt32(ctx, &fd, argv[0])) return JS_EXCEPTION;
+    JSValue value = JS_EXCEPTION;
+    const char *node = JS_ToCString(ctx, argv[0]);
+    if (!node) goto ret;
+    const char *service = JS_ToCString(ctx, argv[1]);
+    if (!service) goto free_node;
     int backlog;
-    if (JS_ToInt32(ctx, &backlog, argv[1])) return JS_EXCEPTION;
-    int rc = listen(fd, backlog);
-    if (rc < 0) return JS_ThrowInternalError(ctx, "listen(%i, %i): %m", fd, backlog);
-    return JS_UNDEFINED;
+    if (JS_ToInt32(ctx, &backlog, argv[2])) goto free_service;
+    const struct addrinfo hints = {
+        .ai_flags = AI_PASSIVE,
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM,
+        .ai_protocol = 0,
+        .ai_addrlen = 0,
+        .ai_addr = NULL,
+        .ai_canonname = NULL,
+        .ai_next = NULL
+    };
+    struct addrinfo *ret, *rp = NULL;
+    if (getaddrinfo(node, service, &hints, &ret) < 0) { value = JS_ThrowInternalError(ctx, "getaddrinfo: %m"); goto free_service; }
+    int fd = -1;
+    for (rp = ret; rp; rp = rp->ai_next) {
+        fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (fd < 0) continue;
+        int value = 1;
+        if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(value)) < 0) goto close_fd;
+        if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &value, sizeof(value)) < 0) goto close_fd;
+        if (!bind(fd, rp->ai_addr, rp->ai_addrlen)) break;
+close_fd:
+        close(fd);
+    }
+    if (fd < 0 || !rp) { value = JS_ThrowInternalError(ctx, "bind: %m"); goto free_service; }
+    if (listen(fd, backlog) < 0) { value = JS_ThrowInternalError(ctx, "listen: %m"); goto free_service; }
+    value = js_sockaddr_to_value(ctx, rp->ai_addr);
+    if (JS_IsException(value)) goto free_service;
+    JS_DefinePropertyValueStr(ctx, value, "fd", JS_NewInt32(ctx, fd), JS_PROP_C_W_E);
+    freeaddrinfo(ret);
+free_service:
+    JS_FreeCString(ctx, service);
+free_node:
+    JS_FreeCString(ctx, node);
+ret:
+    return value;
 }
 
 static JSValue js_loop(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
@@ -205,45 +174,18 @@ static JSValue js_setsockopt(JSContext *ctx, JSValueConst this_val, int argc, JS
     return JS_UNDEFINED;
 }
 
-static JSValue js_socket(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
-    int domain;
-    if (JS_ToInt32(ctx, &domain, argv[0])) return JS_EXCEPTION;
-    int type;
-    if (JS_ToInt32(ctx, &type, argv[1])) return JS_EXCEPTION;
-    int protocol;
-    if (JS_ToInt32(ctx, &protocol, argv[2])) return JS_EXCEPTION;
-    int fd = socket(domain, type, protocol);
-    if (fd < 0) return JS_ThrowInternalError(ctx, "socket(%i, %i, %i): %m", domain, type, protocol);
-    return JS_NewInt32(ctx, fd);
-}
-
 #define countof(x) (sizeof(x) / sizeof((x)[0]))
 
 static const JSCFunctionListEntry js_http_funcs[] = {
     JS_CFUNC_DEF("accept", 1, js_accept),
-    JS_CFUNC_DEF("bind", 3, js_bind),
+    JS_CFUNC_DEF("getpeername", 1, js_getpeername),
     JS_CFUNC_DEF("getsockname", 1, js_getsockname),
     JS_CFUNC_DEF("listen", 2, js_listen),
     JS_CFUNC_DEF("loop", 0, js_loop),
     JS_CFUNC_DEF("recv", 3, js_recv),
     JS_CFUNC_DEF("send", 3, js_send),
-    JS_CFUNC_DEF("setsockopt", 4, js_setsockopt),
-    JS_CFUNC_DEF("socket", 3, js_socket),
 #define DEF(x) JS_PROP_INT32_DEF(#x, x, JS_PROP_CONFIGURABLE )
-    DEF(AF_INET),
-    DEF(AF_INET6),
-    DEF(AF_UNSPEC),
-    DEF(IPPROTO_TCP),
-    DEF(SOCK_STREAM),
-    DEF(SO_KEEPALIVE),
-    DEF(SOL_SOCKET),
     DEF(SOMAXCONN),
-    DEF(SO_REUSEADDR),
-    DEF(SO_REUSEPORT),
-    DEF(STDERR_FILENO),
-    DEF(STDIN_FILENO),
-    DEF(STDOUT_FILENO),
-    DEF(TCP_NODELAY),
 };
 
 static int js_http_init(JSContext *ctx, JSModuleDef *m) {
--- http.js
+++ http.js
@@ -2,35 +2,26 @@
 import * as http from "http.so"
 import * as os from "os"
 import * as std from "std"
-const server = {
-    host: '0.0.0.0',
-    port: 8080,
-    text: 'Hello, World!',
-    END: '\r\n\r\n',
-    update: () => {
-        server.time = (new Date()).toUTCString()
-        server.rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${server.time}\r\nContent-Type: text/plain\r\nContent-Length: `
-        os.setTimeout(server.update, 1000)
-    },
+const text = 'Hello, World!'
+const END = '\r\n\r\n'
+let time
+let rTEXT
+const update = () => {
+    time = (new Date()).toUTCString()
+    rTEXT = `HTTP/1.1 200 OK\r\nServer: j\r\nDate: ${time}\r\nContent-Type: text/plain\r\nContent-Length: `
+    os.setTimeout(update, 1000)
 }
-server.fd = http.socket(http.AF_INET, http.SOCK_STREAM | http.SOCK_NONBLOCK, 0)
-http.setsockopt(server.fd, http.SOL_SOCKET, http.SO_REUSEADDR, 1)
-http.setsockopt(server.fd, http.SOL_SOCKET, http.SO_REUSEPORT, 1)
-server.family = http.bind(server.fd, server.host, server.port)
-//console.log(JSON.stringify(http))
-http.listen(server.fd, http.SOMAXCONN)
-server.update()
-console.log(JSON.stringify(server))
+update()
+const server = http.listen('0.0.0.0', '8080', http.SOMAXCONN)
+console.log(JSON.stringify({time: time, server: server}))
 os.setReadHandler(server.fd, () => {
     const client = http.accept(server.fd)
-    console.log(JSON.stringify({server: server, client: client}))
-    http.setsockopt(client.fd, http.IPPROTO_TCP, http.TCP_NODELAY, 0)
-    http.setsockopt(client.fd, http.SOL_SOCKET, http.SO_KEEPALIVE, 0)
+    console.log(JSON.stringify({time: time, server: server, client: client}))
     os.setReadHandler(client.fd, () => {
         client.request = http.recv(client.fd, 128, 0)
         if (client.request && client.request.length) {
-            client.response = `${server.rTEXT}${server.text.length}${server.END}${server.text}`
-            console.log(JSON.stringify({server: server, client: client}))
+            client.response = `${rTEXT}${text.length}${END}${text}`
+            console.log(JSON.stringify({time: time, server: server, client: client}))
             os.setWriteHandler(client.fd, () => {
                 client.request = undefined
                 http.send(client.fd, client.response, http.MSG_NOSIGNAL)
